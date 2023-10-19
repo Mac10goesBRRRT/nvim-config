7c75221ece6aa97d61a08431a250cbe3155fa031
leonlux <leonlux@icloud.com>
2023-10-19 22:38:15 +0200

    start on a clean slate

--- lua/Lachstec/core/keymaps.lua
+++
@@ -1,9 +0,0 @@
-vim.g.mapleader = ","
-
-local keymap = vim.keymap
-
--- bindings for nvim tree
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle explorer
--- remap copilot keybinding for accepting suggestion 
-keymap.set("i", "<leader>a", 'copilot#Accept("<CR>")', { expr = true, silent = true })
-
