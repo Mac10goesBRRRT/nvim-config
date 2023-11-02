6c18eb130b780bc8b021c3b4a38b8673e2f8e8f9
leonlux <leonlux@icloud.com>
2023-11-02 15:52:43 +0100

    move plugins to top of init.lua in order to ensure bootstrapping success

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
+require('plugins')
 -- general configuration
 require('config/keymap')
 require('config/colorscheme')
 -- plugins
-require('plugins')
 require('plugins/nvim-tree')
 require('plugins/syntax-highlight')
 require('plugins/mason')
